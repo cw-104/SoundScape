1e161ade9233d2cebcddc5c3dd5943fc8c8145cd
NotDev <christiankilduff@gmail.com>
2024-11-19 13:44:45 -0500

    Skeleton of our model

---
+++ SoundFakeModel/Evaluate.py
@@ -0,0 +1,28 @@
+class SoundScapeModel:
+    def __init__(self):
+        '''
+        Initializes the model
+        '''
+        print("Not Implemented")
+
+    def EvaluateFile(file_path):
+        '''
+        Method: 
+        Evaluates a single audio file for deepfake classification
+        
+        ------------
+        Parameters:
+        file: {str} file path to audio file
+
+        ------------
+        Returns:
+        {Result} result of the evaluation 
+        .classification {str} \"DF\" \"Authentic\"
+        .isDF {bool} is the audio file a deepfake
+        .certaintyClass {str} classification name of audio file
+
+        .rawcertainty {float} raw model results
+        .shiftedcertainty {float} shifted model results according to classification ranges
+        (see Results.py for more information)
+        '''
+        print("Not Implemented")
---
+++ SoundFakeModel/Interface.py
@@ -0,0 +1,13 @@
+# COMMAND LINE INTERFACE
+
+from Evaluate import SoundScapeModel
+
+# This is my idea of how to use the model and evaluate a file
+# could change
+# VVVV
+
+
+# model = SoundScapeModel()
+# model.EvaluateFile("example/path")
+
+
