da7920bf1922455e7198bd63ed3ff3861c817138
NotDev <christiankilduff@gmail.com>
2025-01-31 13:54:22 -0500

    activate

--- SoundFakeModel/init_env.sh
+++ SoundFakeModel/init_env.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Check if the virtual environment directory exists
 has_init=true
 if [ ! -d env ]; then
@@ -15,7 +14,6 @@ source env/bin/activate
 if [ "$has_init" = false ]; then
     echo "Installing dependencies..."
     pip install -r requirements.txt
-fi
 
 echo "Python Environment is ready. (source env/bin/activate)"
 
@@ -32,3 +30,5 @@ echo "brew install bzip2 libgmp-dev libblas-dev libffi-dev libgfortran5 libsqlit
 echo "linux command"
 echo 
 echo "sudo apt install bzip2 libgmp-dev libblas-dev libffi-dev libgfortran5 libsqlite3-dev libz-dev libmpc-dev libmpfr-dev libncurses5-dev libopenblas-dev libssl-dev libreadline-dev tk-dev xz-utils libsox-dev"
+
+fi
