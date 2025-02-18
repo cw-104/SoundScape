96b62a7292b3af77dea82db2aa077decc69e194b
Tim McGinley <timmcginley0325@gmail.com>
2025-02-18 11:13:02 -0500

    AXplain working test

--- XplainTest.py
+++ XplainTest.py
@@ -1,48 +1,36 @@
-## Create an Agent
-import os
-os.environ["AIXPLAIN_API_KEY"] = "9ee1be3db852983c78004eebf5172e6688918b10366edd67e33d7095e1ed18da"
+import requests
+import time
 
-#@title # **Create and run your first AI Agent**
+AIXPLAIN_API_KEY = "d97f1786ec0701d8809259408d07e4f739f0794bd80a7c30c024004597aff085"
+AGENT_ID = "67acb51f56173fdefab4fc62"
+POST_URL = f"https://platform-api.aixplain.com/sdk/agents/{AGENT_ID}/run"
 
-#@markdown AI agents 🤖 are software that use reasoning models, like **language models** (LLM/SLM), and **tools** like APIs or custom code, along with data sources, to reason, plan, reflect, and execute tasks **autonomously**. With some tweaking, they can also learn and self-improve!
+headers = {
+	"x-api-key": AIXPLAIN_API_KEY,
+	"Content-Type": 'application/json'
+}
 
-#@markdown **Name your agent and describe its role and task. The more precise your task description, the better your agent will perform.**
+data = {
+	"query": "hello",
+	# "sessionId": "<SESSIONID_TEXT_DATA>",  # Optional: Specify sessionId from the previous message
+}
 
-Name = "Weather Agent" #@param {type:"string"}
-Instructions = "An agent that answers queries about the current weather." #@param {type:"string"}
+# POST request to execute the agent
+response = requests.post(POST_URL, headers=headers, json=data)
+response_data = response.json()
+request_id = response_data.get("requestId")
 
-#@markdown **Add a tool to your agent. This is the tool ID of the [Open Weather API](https://platform.aixplain.com/discover/model/66f83c216eb563266175e201). Find more in aiXplain's [marketplace](https://platform.aixplain.com/discover).**
+get_url = f"https://platform-api.aixplain.com/sdk/agents/{request_id}/result"
 
-Tool = "66f83c216eb563266175e201" #@param {type:"string"}
-Format = "The input query of this tool must be of the form 'text': 'City'." #@param {type:"string"}
-
-from aixplain.factories import AgentFactory
-from aixplain.modules.agent.tool.model_tool import ModelTool
-
-agent = AgentFactory.create(
-	name=Name,
-	description = "Agent for weather detection",
-	instructions=Instructions,
-	tools=[
-		ModelTool(model=Tool, description=Format),
-	],
-) # default LLM is GPT4o
-
-
-#@markdown **Run and test your agent**
-
-Query = "What is the weather in Liverpool, UK?" #@param {type:"string"}
-
-agent_response = agent.run(Query)
-agent_response['data']['output']
-
-#agent.deploy() #to deploy your agent on aiXplain's platform.
-
-#@markdown **To learn how to debug your agent, build a team agent, and deploy your agent, follow along.**
-
-#@markdown **Ask a follow up questions**
-
-Query = "Is that too cold for shorts?" #@param {type:"string"}
-session = agent_response["data"]["session_id"]
-agent_response = agent.run(Query,session_id=session)
-agent_response['data']['output']
+# Polling loop: GET request until the result is completed
+while True:
+	get_response = requests.get(get_url, headers=headers)
+	result = get_response.json()
+	
+	if result.get("completed"):
+		print(result)
+		break
+	else:
+			time.sleep(5) # Wait for 5 seconds before checking the result again
+   
+print("test")
