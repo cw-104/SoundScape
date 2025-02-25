018a47dbd48dfa742a408aa88536f815c2989dfa
Tim McGinley <timmcginley0325@gmail.com>
2025-02-25 13:38:11 -0500

    Vocoder Test

--- src/sound_scape/backend/Models.py
+++ src/sound_scape/backend/Models.py
@@ -30,12 +30,12 @@ class vocoder:
         pred = None
         # Flipped
         if(binary[0] > binary[1]):
-            print("Fake")
-            label = "Fake"
-            pred = binary[0]
-        else:
             print("Real")
             label = "Real"
+            pred = binary[0]
+        else:
+            print("Fake")
+            label = "Fake"
             pred = binary[1]
         
         print('Multi classification result : gt:{}, wavegrad:{}, diffwave:{}, parallel wave gan:{}, wavernn:{}, wavenet:{}, melgan:{}'.format(multi[0], multi[1], multi[2], multi[3], multi[4], multi[5], multi[6]))
